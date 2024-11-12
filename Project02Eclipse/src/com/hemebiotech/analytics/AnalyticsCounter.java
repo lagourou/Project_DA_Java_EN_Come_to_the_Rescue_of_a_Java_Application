eaf121132bd66938d151130afbc46493abf4d58f
lagourou <169063717+lagourou@users.noreply.github.com>
2024-11-12 08:53:45 +0100

    Update AnalyticsCounter.java
    Correction de l'orthographe de chaque commentaires

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,13 +3,13 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.IOException; //Importe la classe pour gérer les exceptions
-import java.util.Map; //importer la collection Map
-import java.util.HashMap; //importer la collection HashMap
+import java.io.IOException; // Importe la classe pour gérer les exceptions
+import java.util.Map; // Importe la collection Map
+import java.util.HashMap; // Importe la collection HashMap
 
  public class AnalyticsCounter {
 	private static Map<String, Integer>symptomCounts = new HashMap<>(); 
-	//Déclaration de la Map pour compter les occurrences de chaque symptomes.
+	// Déclaration de la Map pour compter les occurrences de chaque symptomes.
 
 	// initialize all counters to 0
 	private static int headacheCount = 0;	
@@ -19,7 +19,7 @@ import java.util.HashMap; //importer la collection HashMap
 	public static void main(String args[]) throws Exception {
 		// first get input
 
-		//Ajouter le bloc try pour fermer la ressource BufferedReader
+		// Ajouter le bloc try pour fermer la ressource BufferedReader
 		try(BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"))){
 			
 		String line = reader.readLine(); 
@@ -34,39 +34,39 @@ import java.util.HashMap; //importer la collection HashMap
 			else if (line.equals("rash")) {
 				rashCount++;
 				System.out.println("number of rash: " + rashCount);
-                 //afficher le résultat du nombre de symptomes rash
+                 // afficher le résultat du nombre de symptome rash
 			}
 			else if (line.contains("pupils")) {
 				pupilCount++;
 				System.out.println("number of pupils: " + pupilCount);
-                //afficher le résultat du nombre de symptomes rash
+                // afficher le résultat du nombre de symptome pupil
 
 			} else {
 				SymptomCounts.put(line, SymptomCounts.getOrDefault(line, 0) + 1);
-				//Compte les occurences de symptomes autres que headache, rash et pupil
+				//Compter les occurences de symptomes autres que headache, rash et pupil
 			}
 
 			line = reader.readLine();	// get another symptom
 		}
-		//Ajouter le bloc catch pour gérer les exceptions au cas ou il y a une erreur.
+		// Ajouter le bloc catch pour gérer les exceptions au cas ou il y a une erreur.
 	   } catch (IOException e) {
 		e.printStackTrace();
 	}
 		// next generate output
 
 
-		//Ajouter le bloc try pour fermer la ressource FileWriter
+		// Ajouter le bloc try pour fermer la ressource FileWriter
 		try(FileWriter writer = new FileWriter ("result.out")){
-	    writer.write("headache: " + headacheCount + "\n");
+	        writer.write("headache: " + headacheCount + "\n");
 		writer.write("rash: " + rashCount + "\n");
 		writer.write("dialated pupils: " + pupilCount + "\n");
 		
-		//Écrit les autres symptômes et leurs occurrences dans le fichier.
+		//Écrivez les autres symptômes et leurs occurrences dans le fichier.
 		for (Map.Entry<String, Integer> entry : symptomCounts.entrySet()) {
 			writer.write(entry.getKey() + ":" + entry.getValue() +"\n");
 		}
 		
-		//Ajouter le bloc catch pour gérer les exceptions au cas ou il y a une erreur.
+		// Ajouter le bloc catch pour gérer les exceptions au cas oú il y a une erreur.
 	} catch (IOException e) {
 		e.printStackTrace();
 	} 
