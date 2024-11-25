45076899455e53ee3d04159f9f365198f870bd06
lagourou <169063717+lagourou@users.noreply.github.com>
2024-11-25 14:22:41 +0100

    Modificattion du code

--- Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -6,16 +6,17 @@ import java.io.IOException;
 
 /**
  * Simple brute force implementation of writing symptom data to a file.
+ * 
+ * @param filepath Store the path to the file containing symptom data.
+ * 
  */
 
 public class WriteSymptomDataToFile implements ISymptomWriter {
 
-   private String filepath;
+   private final String filepath;
 
    /**
     * Constructs a WriteSymptomDataToFile object.
-    * 
-    * @param filepath A path to file with symptom strings in it one per line.
     */
 
    public WriteSymptomDataToFile(String filepath) {
@@ -35,8 +36,8 @@ public class WriteSymptomDataToFile implements ISymptomWriter {
             writer.write(symptom + "\n");
          }
       } catch (IOException e) {
-         e.printStackTrace();
-      }
+         System.out.println("Une erreur s'est produite lors de l'opération de fichier : " + e.getMessage());
+         }
    }
 
    /**
