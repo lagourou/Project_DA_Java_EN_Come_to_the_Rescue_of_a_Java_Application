68b4a24dade74ec6a70a4c672f331e19d4f74209
lagourou <agourou84@gmail.com>
2024-11-25 15:48:08 +0100

    Ajout de la documentation Javadoc générée

--- doc/package-search-index.js
+++ doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.hemebiotech.analytics"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.hemebiotech.analytics"}];updateSearchResults();
